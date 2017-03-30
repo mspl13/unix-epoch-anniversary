93c96c3a7c417d0b15a571a91d2bc484980c6e50
Max Spliethöver <m.spliethoever@gmail.com>
2017-03-30 21:40:20 +0200

    Add UTC date when event happens

--- app/index.js
+++ app/index.js
@@ -7,8 +7,12 @@ import {
 // The DOM timestamp display element
 const domTimestamp = document.getElementById("uea-timestamp");
 
-// Set the DOM timestamp goal display element
-document.getElementById("uea-timestamp-goal").innerHTML = getNextEvent();
+// Set the DOM timestamp goal display elements
+let nextEventTimestamp = getNextEvent();
+const nextEventDate = new Date(nextEventTimestamp * 1000).toUTCString();
+
+document.getElementById("uea-timestamp-goal").innerHTML = nextEventTimestamp;
+document.getElementById("uea-timestamp-goal__date").innerHTML = nextEventDate;
 
 // Refresh certain DOM elements to ensure a live counter
 // TODO: execute setInterval without initial wait
